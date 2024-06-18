2463d429fffaa934567b18cbd3db501104a30b8e
Piotr Brozek <1brozek.peter@gmail.com>
2024-06-18 12:25:48 -0500

    added unknown url routing + jpeg image

--- nodeserver.js
+++ nodeserver.js
@@ -3,8 +3,36 @@ const path = require('path');
 
 const app = express();
 
+//routing
+app.get('/app1', function(req, res) {
+    //  JSON data
+    const jsonData = {
+        name: "app1",
+        value: "44",
+    };
+    // Sending JSON response
+    res.json(jsonData);
+});
+
+app.get('/app2', function(req, res) {
+    //  JSON data
+    const jsonData = {
+        name: "app2",
+        value: "54",
+    };
+    // Sending JSON response
+    res.json(jsonData);
+});
+
+
+
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
+
+//catch all unknown URLs
+app.use((req, res) => {
+    res.status(404).json({ error: "Not Found, invalid url" });
+});
 
 const PORT = process.env.PORT || 5000;
 
